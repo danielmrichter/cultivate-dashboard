5d5d2d9d1c6b81395ad64f3258514e73f16a7033
Elwood Nelson <elwoodnelson@gmail.com>
2024-10-04 15:04:35 -0500

    have snackbar and alert modal working

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,9 +13,10 @@ import SiteManagerView from "../SiteManagerView/SiteManagerView.jsx";
 import Header from "../Header/Header.jsx";
 import RedirectComponent from "../RedirectComponent/RedirectComponent.jsx";
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute.jsx'
-import TemperatureWarning from "../AlertComponents/Snackbar.jsx";
+import TemperatureWarning from "../AlertComponents/WarningSnackbar.jsx";
 import AlertHistory from "../AlertHistory/AlertHistory.jsx";
 import PilerView from "../PilerView/PilerView.jsx";
+import AlertCaller from "../AlertComponents/AlertCaller.jsx";
 
 
 function App() {
@@ -31,7 +32,7 @@ function App() {
     <Router>
       <Box sx={{backgroundColor: "#E5E5E5"}}>
       <Header />
-      {/* <TemperatureWarning /> */}
+      <AlertCaller />
       <Switch>
         <Route exact path="/">
           {user.id ? <Redirect to="/user" /> : <Redirect to="/login" />}
--- src/components/Nav/GenManagerNav.jsx
+++ src/components/Nav/GenManagerNav.jsx
@@ -1,36 +1,51 @@
-import * as React from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Drawer, List, ListItemButton, ListItemText, Collapse, Box} from '@mui/material';
-import ListSubheader from '@mui/material/ListSubheader';
-import { useState } from 'react';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import { useDispatch, useSelector } from 'react-redux';
-import MenuIcon from '@mui/icons-material/Menu';
-import { useHistory, useEffect } from 'react-router-dom/cjs/react-router-dom.min';
-import LogOutButton from '../AccountComponents/LogOutButton/LogOutButton';
-
-
+import { useEffect } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Drawer,
+  List,
+  ListItemButton,
+  ListItemText,
+  Collapse,
+  Box,
+} from "@mui/material";
+import ListSubheader from "@mui/material/ListSubheader";
+import { useState } from "react";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { useDispatch, useSelector } from "react-redux";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  useHistory,
+} from "react-router-dom/cjs/react-router-dom.min";
+import LogOutButton from "../AccountComponents/LogOutButton/LogOutButton";
 
 export default function GenManagerNav() {
   const [drawerOpen, setDrawerOpen] = useState(false);
   const [nestedOpen, setNestedOpen] = useState(true);
   const user = useSelector((store) => store.user);
-  const siteList = useSelector((store) => store.siteList)
+  const siteList = useSelector((store) => store.siteList);
   const history = useHistory();
   const dispatch = useDispatch();
 
-  React.useEffect(() => {
-    dispatch({type: 'GET_SITE_LIST'})
-  },[]);
-    // open or close the Drawer
+  useEffect(() => {
+    dispatch({ type: "GET_SITE_LIST" });
+  }, []);
+  // open or close the Drawer
   const toggleDrawer = (open) => (event) => {
-    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
       return;
     }
     setDrawerOpen(open);
   };
 
-   // expand or condense the collapsible list
+  // expand or condense the collapsible list
   const handleNestedClick = () => {
     setNestedOpen(!nestedOpen);
   };
@@ -44,12 +59,12 @@ export default function GenManagerNav() {
     <Box
       sx={{ width: 160 }}
       role="presentation"
-    //   onClick={toggleDrawer(false)}   
-    //   onKeyDown={toggleDrawer(false)}
-    //   having them here closes the Drawer on any subsequent click
+      //   onClick={toggleDrawer(false)}
+      //   onKeyDown={toggleDrawer(false)}
+      //   having them here closes the Drawer on any subsequent click
     >
       <List
-        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
         component="nav"
         aria-labelledby="nested-list-subheader"
         // subheader={
@@ -58,44 +73,66 @@ export default function GenManagerNav() {
         //   </ListSubheader>
         // }
       >
-          {/* Dashboard list item */}
-        <ListItemButton key="dashboard" >
-          <ListItemText primary="ADMIN DASHBOARD"
-            onClick={() =>  { handleNavigation(`/admin`);setDrawerOpen(false);}}
-            onKeyDown={toggleDrawer(false)}/>
+        {/* Dashboard list item */}
+        <ListItemButton key="dashboard">
+          <ListItemText
+            primary="ADMIN DASHBOARD"
+            onClick={() => {
+              handleNavigation(`/admin`);
+              setDrawerOpen(false);
+            }}
+            onKeyDown={toggleDrawer(false)}
+          />
         </ListItemButton>
 
-          {/* Site list item */}
-        <ListItemButton key="sites"
-            onClick={handleNestedClick} sx={{backgroundColor:'#F1F1F1'}}>
+        {/* Site list item */}
+        <ListItemButton
+          key="sites"
+          onClick={handleNestedClick}
+          sx={{ backgroundColor: "#F1F1F1" }}
+        >
           <ListItemText primary="SITES" />
           {nestedOpen ? <ExpandLess /> : <ExpandMore />}
         </ListItemButton>
 
-           {/* sites mapped into the collapsible list */}
+        {/* sites mapped into the collapsible list */}
         <Collapse in={nestedOpen} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
-            {siteList[0] && siteList.map((site) => {
-              return(
-              <ListItemButton sx={{ pl: 4, backgroundColor:'#F1F1F1'}} 
-                  key={site.id} >
-                  <ListItemText primary={site.site}  
-                      onClick={() => {   {/* collapses the Drawer and goes to site page */}
+            {siteList[0] &&
+              siteList.map((site) => {
+                return (
+                  <ListItemButton
+                    sx={{ pl: 4, backgroundColor: "#F1F1F1" }}
+                    key={site.id}
+                  >
+                    <ListItemText
+                      primary={site.site}
+                      onClick={() => {
+                        {
+                          /* collapses the Drawer and goes to site page */
+                        }
                         setDrawerOpen(false);
-                        handleNavigation(`/site/${site.id}`)}}
-                      />
-              </ListItemButton>
-            )})}
+                        handleNavigation(`/site/${site.id}`);
+                      }}
+                    />
+                  </ListItemButton>
+                );
+              })}
           </List>
         </Collapse>
-            {/* Alerts list item */}
+        {/* Alerts list item */}
         <ListItemButton key="users">
-          <ListItemText primary="USER LIST"
-            onClick={() => {setDrawerOpen(false);handleNavigation(`/user-list`)}}
-            onKeyDown={toggleDrawer(false)}/>
+          <ListItemText
+            primary="USER LIST"
+            onClick={() => {
+              setDrawerOpen(false);
+              handleNavigation(`/user-list`);
+            }}
+            onKeyDown={toggleDrawer(false)}
+          />
         </ListItemButton>
 
-           {/* /Trends list item */}
+        {/* /Trends list item */}
         {/* <ListItemButton key="users">
           <ListItemText primary="TRENDS"
             onClick={() => {setDrawerOpen(false);handleNavigation(`/trends`)}}
@@ -119,10 +156,14 @@ export default function GenManagerNav() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, textAlign: "center" }}
+          >
             Cultivate General Manager
           </Typography>
-      <LogOutButton />
+          <LogOutButton />
         </Toolbar>
       </AppBar>
 
@@ -132,13 +173,13 @@ export default function GenManagerNav() {
         onClose={toggleDrawer(false)}
         variant="temporary"
         sx={{
-          '& .MuiDrawer-paper': {
-            marginTop: '64px', // keeps the Drawer below the AppBar
+          "& .MuiDrawer-paper": {
+            marginTop: "64px", // keeps the Drawer below the AppBar
             width: 160,
           },
         }}
         ModalProps={{
-          keepMounted: true, // Improves performance when Drawer is hidden  
+          keepMounted: true, // Improves performance when Drawer is hidden
         }}
       >
         {drawerList}
