dfcddcebd800f00b66d9355abe25bf80afab233d
Daniel Richter <drichter3697@gmail.com>
2024-10-15 09:36:25 -0500

    defined basic config settings for a PWA

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa'
+
+// This is the WIP PWA Configuration. This is using a plugin
+// called vite-pwa. It uses vite's build to generate a manifest and all the
+// necessary service workers. It's not implemented since some implementation
+// details are beyond the scope of this app.
+//  Read more: https://vite-pwa-org.netlify.app/guide/
+
+// This is the current config. It goes in the plugins property, as part of the plugins.
+// VitePWA({registerType: 'autoUpdate', injectRegister: 'inline'})
 
 export default defineConfig(() => {
     return {
@@ -11,6 +21,6 @@ export default defineConfig(() => {
                 "/api":'http://localhost:5001',
             }
         },
-        plugins: [react()],
+        plugins: [react(), ]
     };
 });
